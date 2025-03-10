36b58df40b54c1c721e446cde487202ff42a41cf
sajid713 <sajidkhanali5599@gmail.com>
2025-03-10 13:59:10 +0500

    added the main.py file in the project

---
+++ main.py
@@ -0,0 +1,11 @@
+docs = \
+"""
+Artificial Intelligence
+    - 7th Semester Project
+    - Host on the web,
+    - use Google Colab for model training
+    - use Github for code saving. 
+"""
+
+if __name__ == "__main__":
+    print(docs)
